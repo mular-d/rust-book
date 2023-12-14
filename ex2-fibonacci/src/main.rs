6c5e9b2557dcfb0576c90c072e381526f2dbeca1
Muluneh Derese <mulunehd@ethiopianairlines.com>
2023-12-14 14:31:29 +0300

    feat: ex2 fibonacci

---
+++ ex2-fibonacci/src/main.rs
@@ -0,0 +1,21 @@
+use std::io;
+
+fn main() {
+    println!("Enter the number: ");
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let input: i32 = input.trim().parse().expect("Invalid input.");
+    println!("Fibonacci of {input} is: {}", fibonacci(input));
+}
+
+fn fibonacci(input: i32) -> i32 {
+    if input <= 2 {
+        return 1;
+    } else {
+        return fibonacci(input - 1) + fibonacci(input - 2);
+    }
+}
