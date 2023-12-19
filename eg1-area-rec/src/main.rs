1490ea03421364c40bdef52763628aa070148157
Muluneh Derese <mulunehd@ethiopianairlines.com>
2023-12-19 12:10:37 +0300

    feat: eg-1 use method syntax

--- eg1-area-rec/src/main.rs
+++ eg1-area-rec/src/main.rs
@@ -3,22 +3,49 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        if self.height > other.height && self.width > other.width {
+            return true;
+        }
+        return false;
+    }
+    // associated functions
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
-    let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
 
-    dbg!(&rect1);
-    println!("rect1 is: {:#?}", rect1);
-
     println!(
         "The area of the rectangle is {} square pixels",
-        area(&rect1)
+        rect1.area()
     );
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let _sq = Rectangle::square(3);
 }
