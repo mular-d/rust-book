c4ad25b37a3e67ab265ea50f9912eebd4fdb3eb7
Muluneh Derese <mulunehd@ethiopianairlines.com>
2023-12-22 17:13:11 +0300

    feat: ch7 package module and crate examples

---
+++ ch7-restaurant/src/lib.rs
@@ -0,0 +1,57 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
+fn deliver_order() {}
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+    fn cook_order() {}
+
+    mod back_of_house {
+        pub enum Appetizer {
+            Soup,
+            Salad,
+        }
+    }
+
+    pub fn eat_at_restaurant() {
+        let _order1 = back_of_house::Appetizer::Soup; // fields of enum are public by default
+        let _order2 = back_of_house::Appetizer::Salad;
+    }
+}
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // meal.seasonal_fruit = String::from("blueberries"); // doesn't work cause its a private field
+}
