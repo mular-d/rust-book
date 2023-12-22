a87b2de20138f7a4224e101a7ad468f84be712d4
Muluneh Derese <mulunehd@ethiopianairlines.com>
2023-12-22 10:47:30 +0300

    feat: ch6 match control flow

--- ch6/src/main.rs
+++ ch6/src/main.rs
@@ -13,8 +13,8 @@ fn main() {
     route(four);
     route(six);
 
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));
+    let _home = IpAddr::V4(127, 0, 0, 1);
+    let _loopback = IpAddr::V6(String::from("::1"));
 
     enum Message {
         Quit,
@@ -31,6 +31,71 @@ fn main() {
 
     let m = Message::Write(String::from("hello"));
     m.call();
+
+    #[derive(Debug)]
+    enum UsState {
+        Alabama,
+        Alaska,
+        // --snip--
+    }
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState),
+    }
+
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("State quarter from {:?}!", state);
+                25
+            }
+        }
+    }
+
+    // control flow with if let
+    let coin: Coin = Coin::Quarter(UsState::Alaska);
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+    println!("Count of all quarter is: {}", count);
+
+    // matching with Option<T>
+    let five = Some(5);
+    let _six = plus_one(five);
+    let _none = plus_one(None);
+
+    // catch-all patterns
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+        // _ => reroll(), // catch-all but don't use the value
+        // _ => (), // catch-all but dont do anything
+    }
 }
 
-fn route(ip_kind: IpAddrKind) {}
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(_num_spaces: u8) {}
+// fn reroll() {}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn route(_ip_kind: IpAddrKind) {}
