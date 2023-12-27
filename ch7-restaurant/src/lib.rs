1e5ae70bae4bc8baf604b0a3c95e755920b85dbd
Muluneh Derese <mulunehd@ethiopianairlines.com>
2023-12-27 12:06:40 +0300

    feat: ch7 separating module into different files

--- ch7-restaurant/src/lib.rs
+++ ch7-restaurant/src/lib.rs
@@ -1,22 +1,16 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        fn seat_at_table() {}
-    }
+mod front_of_house;
 
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
-}
+use crate::front_of_house as front;
+pub use crate::front_of_house::hosting;
+
 fn deliver_order() {}
+
 mod back_of_house {
+    // making structs and enums public
     pub struct Breakfast {
         pub toast: String,
         seasonal_fruit: String,
     }
-
     impl Breakfast {
         pub fn summer(toast: &str) -> Breakfast {
             Breakfast {
@@ -26,6 +20,7 @@ mod back_of_house {
         }
     }
 
+    // starting relative paths with super
     fn fix_incorrect_order() {
         cook_order();
         super::deliver_order();
@@ -44,10 +39,16 @@ mod back_of_house {
         let _order2 = back_of_house::Appetizer::Salad;
     }
 }
-pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
 
-    front_of_house::hosting::add_to_waitlist();
+pub fn eat_at_restaurant() {
+    // Absolute path
+    hosting::add_to_waitlist();
+
+    // Relative path
+    hosting::add_to_waitlist();
+
+    // local name alias
+    front::hosting::add_to_waitlist();
 
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
