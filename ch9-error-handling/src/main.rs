97d00b634a7f4ee468c40ae90ba939fd9ab3c5b8
Muluneh Derese <mulunehd@ethiopianairlines.com>
2024-01-03 15:22:37 +0300

    feat: ch9 error handling

---
+++ ch9-error-handling/src/main.rs
@@ -0,0 +1,117 @@
+use std::fs::File;
+use std::io::{ErrorKind, Read};
+use std::path::Component::ParentDir;
+use std::{fs, io};
+
+fn main() {
+    // panic!("Hello, world!");
+    // let v = vec![1, 2, 3];
+
+    // v[99];
+
+    let greeting_file_result = File::open("hello.txt");
+
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => panic!("Problem opening the file: {:?}", error),
+    // };
+
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem creating the file: {:?}", e),
+    //         },
+    //         other_error => {
+    //             panic!("Problem opening the file: {:?}", other_error);
+    //         }
+    //     },
+    // };
+
+    let greeting_file = greeting_file_result.unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt")
+                .unwrap_or_else(|error| panic!("Problem creating the file: {:?}", error))
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    let greeting_file = File::open("hello.txt").unwrap();
+    let greeting_file =
+        File::open("hello.txt").expect("hello.txt should be included in this project");
+
+    // enhance the guessing game
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    loop {
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if guess < 1 || guess > 100 {
+            println!("The secret number will be between 1 and 100.");
+            continue;
+        }
+    }
+
+    pub struct Guess {
+        value: i32,
+    }
+    impl Guess {
+        pub fn new(value: i32) -> Guess {
+            if value < 1 || value > 100 {
+                panic!("Guess value must be between 1 and 100, got {}", value);
+            }
+            Guess { value }
+        }
+        pub fn value(&self) -> i32 {
+            self.value
+        }
+    }
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("username.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+// Error propagating using ? operator
+fn read_username_from_file_2() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+// Simplified version
+fn read_username_from_file_3() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+// using the standard lib "fs"
+fn read_username_from_file_4() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
