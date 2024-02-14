59e8b7c8f3aea8a13a410b2738e136d090b6b8cc
Muluneh Derese <muluneh.derese@protonmail.com>
2024-02-14 12:48:29 +0300

    feat: using Box<T>

---
+++ ch15-smart-pointers/src/main.rs
@@ -0,0 +1,14 @@
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+fn main() {
+    let b = Box::new(5);
+    println!("b = {}", b);
+
+    let _list = List::Cons(
+        1,
+        Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))),
+    );
+}
