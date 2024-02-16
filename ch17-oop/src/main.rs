de02acc53dbd30b8fd4d47282c1be7678737aaf9
Muluneh Derese <muluneh.derese@protonmail.com>
2024-02-16 14:41:11 +0300

    feat: oop state pattern

--- ch17-oop/src/main.rs
+++ ch17-oop/src/main.rs
@@ -1,4 +1,4 @@
-use ch17_oop::{AveragedCollection, Button, Screen, SelectBox};
+use ch17_oop::{AveragedCollection, Button, Post, Screen, SelectBox};
 
 fn main() {
     let mut collection = AveragedCollection::new(Vec::new());
@@ -41,4 +41,34 @@ fn main() {
     };
 
     screen.run();
+
+    println!("============================================");
+
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+
+    println!("============================================");
+
+    let mut post = Post::new();
+
+    post.add_text("Some other post for today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.reject();
+    assert_eq!("", post.content());
+
+    post.request_review();
+    post.approve();
+    assert_eq!("Some other post for today", post.content());
 }
